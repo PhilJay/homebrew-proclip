b1f33123586fe810fce636115129a7caa39346f0
Philipp Jahoda <phil.jahoda@gmail.com>
2019-04-21 23:40:54 +0200

    Update formula

--- brew_formula.rb
+++ brew_formula.rb
@@ -3,7 +3,7 @@ class BrewFormula < Formula
   homepage "https://github.com/PhilJay/homebrew-proclip"
 
 
-  url "https://github.com/PhilJay/homebrew-proclip.git", :using => :git, :tag => "0.0.1"
+  url "https://github.com/PhilJay/homebrew-proclip.git", :using => :git, :tag => "0.0.2"
 
   def install
     bin.install "proclip"
