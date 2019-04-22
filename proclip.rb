de401c561f6cf6c68fadb2e05b8ed41b705d02fd
Philipp Jahoda <phil.jahoda@gmail.com>
2019-04-22 20:28:07 +0200

    Update tag

--- proclip.rb
+++ proclip.rb
@@ -3,7 +3,7 @@ class Proclip < Formula
   homepage "https://github.com/PhilJay/homebrew-proclip"
 
 
-  url "https://github.com/PhilJay/homebrew-proclip.git", :using => :git
+  url "https://github.com/PhilJay/homebrew-proclip.git", :using => :git, :tag => "0.1.0"
 
   def install
     # the name of the executable in the git repo
