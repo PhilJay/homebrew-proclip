c2b64ffc5cefb2a31b045d14a52c19e52be2635d
Philipp Jahoda <phil.jahoda@gmail.com>
2020-02-06 07:48:44 +0100

    Update proclip.rb

--- proclip.rb
+++ proclip.rb
@@ -1,3 +1,4 @@
+# Homebrew formula file for deployment
 class Proclip < Formula
   desc "A terminal / commandline extension capable of storing and executing frequently used commands conveniently"
   homepage "https://github.com/PhilJay/homebrew-proclip"
@@ -6,7 +7,7 @@ class Proclip < Formula
   url "https://github.com/PhilJay/homebrew-proclip.git", :using => :git, :tag => "0.1.4"
 
   def install
-    # the name of the executable in the git repo
+    # pcl is the name of the executable in the git repo
     bin.install "pcl"
   end
 end
